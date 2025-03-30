e0149a5157bbdf0dfbc362d208d310a93f6708d5
water <linshy76@mail2.sysu.edu.cn>
2025-03-30 21:00:04 +0800

    fix ApiBus

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -2,21 +2,22 @@ import { defineStore } from 'pinia'
 
 import { ref, unref } from 'vue'
 
-import eventEmitter from '@/utils/eventEmitter'
+import { ApiBus } from '@/utils/eventEmitter'
 
 // 管理token
 export const useUserStore = defineStore('user', () => {
   const token = ref<string>('')
+  const userInfo = ref<UserInfo>()
 
-  eventEmitter.on('TOKEN:GET', (_req, resp) => {
+  ApiBus.on('TOKEN:GET', (_req, resp) => {
     resp.token = token.value
   })
 
-  eventEmitter.on('API:UN_AUTH', () => {
+  ApiBus.on('API:UN_AUTH', () => {
     logout()
   })
 
-  eventEmitter.on('API:LOGOUT', () => {
+  ApiBus.on('API:LOGOUT', () => {
     logout()
   })
 
@@ -24,8 +25,6 @@ export const useUserStore = defineStore('user', () => {
     token.value = ''
     userInfo.value = undefined
   }
-
-  const userInfo = ref<UserInfo>()
 
   const setUser = (user: UserInfo) => {
     const newUser = unref(user)
