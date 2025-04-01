2e5580d66588b95ca3acf93ba1a3fa1502913d74
water <linshy76@mail2.sysu.edu.cn>
2025-04-01 17:39:39 +0800

    移动端导航栏

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -1,27 +1,41 @@
-<script lang="ts" setup></script>
-
-<style scoped>
-.router_link{
-  font-size: larger;
-  padding: 5px 10px;
-  background-color: #e0e0e0;
-  border-radius: 8px;
-}
-.router_link:hover{
-  background-color: #b1aeae;
-}
-</style>
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue/dist/iconify.js'
+</script>
 
 <template>
   <div class="w-full h-full flex flex-col justify-between items-center">
-    <header class="flex flex-row w-full justify-between items-center p-4 shadow-xl">
+    <header class="flex flex-row w-full justify-between items-center p-4 shadow-2xl">
       <h1 class="w-fit">ChineseChess</h1>
       <nav class="w-fit hidden sm:block">
         <ul class="flex flex-row gap-4 mr-6">
-          <router-link to="/" class="router_link">Home</router-link>
-          <router-link to="/about" class="router_link">About</router-link>
-          <router-link to="/room" class="router_link">Room</router-link>
+          <router-link to="/" class="text-xl p-4 rounded-lg bg-[#e0e0e0]" hover="bg-[#b1aeae]"
+            >Home</router-link
+          >
+          <router-link to="/about" class="text-xl p-4 rounded-lg bg-[#e0e0e0]" hover="bg-[#b1aeae]"
+            >About</router-link
+          >
+          <router-link to="/room" class="text-xl p-4 rounded-lg bg-[#e0e0e0]" hover="bg-[#b1aeae]"
+            >Room</router-link
+          >
         </ul>
+      </nav>
+      <nav class="group sm:hidden relative">
+        <Icon width="25" height="25" icon="ion:navicon-round" />
+        <div
+          class="hidden absolute flex-col -translate-x-3/5 bg-white shadow-lg rounded-lg p-4 gap-2 z-10"
+          group-focus="flex"
+          group-hover="flex"
+        >
+          <router-link to="/" class="text-xl p-4 rounded-lg bg-[#e0e0e0]" hover="bg-[#b1aeae]"
+            >Home</router-link
+          >
+          <router-link to="/about" class="text-xl p-4 rounded-lg bg-[#e0e0e0]" hover="bg-[#b1aeae]"
+            >About</router-link
+          >
+          <router-link to="/room" class="text-xl p-4 rounded-lg bg-[#e0e0e0]" hover="bg-[#b1aeae]"
+            >Room</router-link
+          >
+        </div>
       </nav>
     </header>
     <RouterView />
